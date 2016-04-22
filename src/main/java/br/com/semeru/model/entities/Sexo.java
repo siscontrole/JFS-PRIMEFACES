fa9483addb347bc728b4984126f960358eb7322c
engenharia <engenharia@192.168.0.10>
2016-04-22 13:13:13 -0300

    Adição da entidade sexo.

---
+++ src/main/java/br/com/semeru/model/entities/Sexo.java
@@ -0,0 +1,84 @@
+package br.com.semeru.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+@Entity
+@Table(name="sexo")
+public class Sexo implements Serializable {
+    private static final long serialVesrionUID = 1L;
+    @Id
+    @GeneratedValue
+    @Column(name="idSexo", nullable = false)
+    private Integer idSexo;
+    @Column(name="Descricao", unique = true, nullable = false, length = 9)
+    private String descricao;
+    
+    @OneToMany(mappedBy = "sexo", fetch = FetchType.LAZY)
+    @ForeignKey(name = "PessoaSexo")
+    private List<Pessoa> pessoas;
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+    
+    public Sexo() {
+    }
+
+    public Integer getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(Integer idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.idSexo);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sexo other = (Sexo) obj;
+        if (!Objects.equals(this.idSexo, other.idSexo)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+}
